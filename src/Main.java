135c7dde0fa150d9a21d9107bf4e257208bcf6f7
kasra nik <kasranikshabani@yahoo.com>
2023-11-05 16:14:27 +0330

    CHapter12

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,112 @@
+import java.awt.*;
+import java.util.Arrays;
+import java.util.Date;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        //-----------FirstTothree--------------
         //System.out.println("Hello World");
-        int my_age = 24;
-        int her_age = my_age;
-        System.out.println(her_age);
-        }
+//        int my_age = 24;
+//        int her_age = my_age;
+//        System.out.println(her_age);
+
+
+        //-----------primitive types--------------
+//        byte age = 8 ;
+//        short temp = 28 ;
+//        int stock = 3_485_236;
+//        long phone = 913_816_2670L;
+//        double score = 15.25;
+//        float mathscore = 16.75f;
+//        char character = '4';
+//        boolean form = false;
+//
+//        System.out.println(phone);
+
+
+        //-----------reference types--------------
+
+//        Date now = new Date();
+//        System.out.println(now);
+
+
+        //-----------مقایسه primitive type ها و reference type ها--------------
+
+//        int x = 1;
+//        int y = x;
+//        x = 2;
+//        System.out.println("x = "+x);
+//        System.out.println("y = "+y);
+
+
+//        Point point1 = new Point(1,1) ;
+//        Point point2 = point1;
+//        point1.x=2;
+//        point1.y=5;
+//
+//        System.out.println("point1 is "+point1);
+//        System.out.println("point2 is "+point2);
+
+
+        //-----------کلاس String--------------
+//        String hello = new String("Hello world");
+//        System.out.println(hello);
+//
+//        String hello2 = "Hello2 world";
+//        boolean isStrWith = hello2.startsWith("H");
+//        boolean isEndWith = hello2.startsWith("L");
+//        boolean isIn = hello2.contains("e");
+//        String Email = "";
+//        System.out.println(isStrWith);
+//        System.out.println(isEndWith);
+//        System.out.println(isIn);
+//        System.out.println(Email.isEmpty());
+//        System.out.println(hello2.length());
+//        System.out.println(hello2.indexOf("H"));
+//        System.out.println(hello2.charAt(3));
+//        System.out.println(hello2.toLowerCase());
+//        System.out.println(hello2.toUpperCase());
+//        System.out.println(hello2.substring(0,5));
+//        System.out.println(hello2.replace('l','*'));
+
+
+        //----------------- Array -----------------
+//        int[] ages = {20,14,28};
+//        Arrays.sort(ages);
+//        System.out.println(Arrays.toString(ages));
+//        System.out.println(ages.length);
+//
+//        int[][] people = {{150,24},{175,25},{190,32}};
+//        System.out.println(Arrays.deepToString(people));
+//
+//        int[][][] people2 = {
+//                {{1,2,3,4},{4,8,9,6}},
+//                {{7,5,68,6},{4,456,52,}},
+//                {{65,85,96,3},{121,3,4,8}}
+//        };
+
+
+
+        //--------------------- constants ----------------------
+//        final float PI = 3.14f; //constant
+//        int r = 4;
+//        float S = PI * r * r ;
+//        System.out.println(S);
+
+
+
+        //--------------------- operatores ----------------------
+
+        int x = 15;
+        System.out.println(x + 5);
+        System.out.println(x - 5);
+        System.out.println(x * 5);
+        System.out.println(x / 5);
+        System.out.println(x % 5);
+
+
+
+    }
 }
